f8d9b69f9ab96214d40f64081b1a6eaacbdbfb28
sick-carlito <carlb1319@gmail.com>
2020-05-10 16:07:09 +0300

    Completed sign_up/sign_in functionality

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,10 +5,10 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
 
-  validates :fullname, presence: true, length: { maximum: 20 }
-  validates :username, presence: true, length: { maximum: 20 }
+  validates :name, presence: true, length: { maximum: 20 }
+  
 
-  validates_uniqueness_of :username
+  validates_uniqueness_of :name
 
   has_many :posts
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   root 'posts#index'
-  devise_for :users
+  devise_for :users, :controllers => { registrations: 'registrations' }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  resources :users, only: [:create, :show, :edit, :top, :destroy]
+  resources :users, only: [:create, :show, :edit, :destroy]
   resources :posts, only: [:index, :create]
   
 end
